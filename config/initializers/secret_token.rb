8059ba5c64b8bc9a7e877d34fe58e694ab71e8b7
Kelly Packer <kelly@creativesoapbox.com>
2013-01-20 14:59:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kellypacker::Application.config.secret_token = '9ef21f92a6bab08fbd5e1d44bb344f77b348181a18dbba9beb08a69ef1c80a35a2e5c4dcdad5a23c659285989a8787d159c1d457e353779c2579c2d9dea4fe52'
