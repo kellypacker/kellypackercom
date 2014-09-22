10d661805b0cca72471a7ebd8284d11ab45221fe
Kelly Packer <kelly@creativesoapbox.com>
2014-09-21 22:38:04 -0600

    misc

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -2,5 +2,7 @@ class ArtworksController < ApplicationController
 
   def show
     @artwork = Artwork.find(params[:id])
+    @page_title = @artwork.title
+    @page_desc = "Artwork by Kelly Packer: #{@artwork.title}, #{@artwork.medium.name}, #{@artwork.year}"
   end
 end
