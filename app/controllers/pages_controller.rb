15ea11b72027cf8e9249db3601846f32b0841b54
Kelly Packer <kelly@creativesoapbox.com>
2015-01-13 23:28:38 -0700

    updating location

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,19 @@
 class PagesController < ApplicationController
   def contact
     @body_class="contact"
-    @page_title = "Contact Kelly Packer Artist & Web Developer in Boise, Idaho"
-    @page_desc = "Contact Kelly Packer regarding general inquires, web development project and artwork commissions."
+    @page_title = "Contact Kelly Packer artist & web developer in Butte, Montana"
+    @page_desc = "Contact Kelly Packer regarding general inquires, web development projects and artwork commissions."
   end
 
   def web_development
     @body_class ="web-development"
-    @page_title = "Kelly Packer Web Developer in Boise, Idaho"
-    @page_desc = "Kelly Packer is a web devloper in Boise, Idaho specializing in Javascript and Ruby on Rails. Experienced with Backbone.js, Angular.js, HTML5."
+    @page_title = "Kelly Packer web developer in Butte, Montana"
+    @page_desc = "Kelly Packer is a web developer living in Butte, Montana specializing in Javascript and Ruby on Rails. Experienced with Backbone.js, Angular.js, HTML5."
   end
 
   def profile
     @body_class="profile"
-    @page_title = "Kelly Packer is an Artist & Web Developer in Boise, Idaho"
-    @page_desc = "Profile of Kelly Packer, artist and web developer in Boise, Idaho. Includes influences, articles and artist statement."
+    @page_title = "Kelly Packer is an artist & web developer in Butte, Montana"
+    @page_desc = "Profile of Kelly Packer, artist and web developer in Butte, Montana. Includes influences, articles and artist statement."
   end
 end
