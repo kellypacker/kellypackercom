eca4678d2b74d11755d3f5268b7dedae129de538
Kelly Packer <kelly@creativesoapbox.com>
2015-03-09 09:34:22 -0600

    removed Boise from meta title

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @page_title = "Artist and Web Developer in Boise, Idaho"
-    @page_desc = "Kelly Packer is an artist and web developer in Boise, Idaho. Features a portfolio of her contemporary abstract artwork. Includes web development projects."
+    @page_title = "Artist and Web Developer"
+    @page_desc = "Kelly Packer is an artist and web developer. Features a portfolio of her contemporary abstract artwork. Includes web development projects."
   end
 end
